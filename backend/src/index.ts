113c1df4e3921f7e9b55d5522ed45254f3600f34
Scorpeex <argentum.nrj@gmail.com>
2024-12-24 22:44:30 +0700

    something wrong with eslint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,15 @@
-import express from 'express'
 import * as trpcExpress from '@trpc/server/adapters/express'
-import { trpcRouter } from './trpc'
 import cors from 'cors'
+import express from 'express'
+import { trpcRouter } from './trpc'
 
-const ideas = [
-  { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Description of idea 1...' },
-  { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Description of idea 2...' },
-  { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Description of idea 3...' },
-  { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Description of idea 4...' },
-  { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
-]
+// const ideas = [
+//   { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Description of idea 1...' },
+//   { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Description of idea 2...' },
+//   { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Description of idea 3...' },
+//   { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Description of idea 4...' },
+//   { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
+// ]
 
 const expressApp = express()
 expressApp.use(cors())
