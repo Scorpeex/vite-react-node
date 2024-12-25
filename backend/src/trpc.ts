c1d9516dcd23dabd76c8408ae27d3e35f62a1e5f
Scorpeex <argentum.nrj@gmail.com>
2024-12-25 22:12:46 +0700

    againn

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -8,8 +8,10 @@ const ideas = [
   { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
 ]
 
-const x : number = 1
-if (Math.random()) console.info(x)
+const x: number = 2
+if (Math.random()) {
+  console.info(x)
+}
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
