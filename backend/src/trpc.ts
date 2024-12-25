f585f4394b1694b5f3a9b7e989a5d9e2d60375a6
Scorpeex <argentum.nrj@gmail.com>
2024-12-25 21:49:46 +0700

    something bad again

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -8,7 +8,7 @@ const ideas = [
   { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
 ]
 
-const x : number = 'x'
+const x : number = 'x1'
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
