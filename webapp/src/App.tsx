6e4799d64d203420ca67b6b191930ecbd4846f55
Scorpeex <argentum.nrj@gmail.com>
2024-12-26 20:56:11 +0700

    Add some routes

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeaPage'
+import { ViewIdeaPage } from './pages/ViewIdeaPage'
+import { getAllIdeasRoute, getViewIdeaRoute } from './lib/routes'
 
 export const App = () => {
   return (
     <TrpcProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+          <Route path={getViewIdeaRoute({ ideaNick: ':ideaNick' })} element={<ViewIdeaPage />} />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   )
 }
