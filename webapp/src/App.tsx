49a5cbb8813328a87e6924fa68172ad541584bdb
Scorpeex <argentum.nrj@gmail.com>
2024-12-26 21:20:44 +0700

    Push all

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeaPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
-import { getAllIdeasRoute, getViewIdeaRoute } from './lib/routes'
+import { getAllIdeasRoute, getViewIdeaRoute, viewIdeaRouteParams } from './lib/routes'
 
 export const App = () => {
   return (
@@ -10,7 +10,7 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
-          <Route path={getViewIdeaRoute({ ideaNick: ':ideaNick' })} element={<ViewIdeaPage />} />
+          <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
         </Routes>
       </BrowserRouter>
     </TrpcProvider>
