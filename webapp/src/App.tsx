9c49baa7990fbe0be7ac8e29f8dfa743a42fbc45
Scorpeex <argentum.nrj@gmail.com>
2025-02-09 17:26:28 +0700

    More auth

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,6 +4,7 @@ import * as routes from './lib/routes'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeaPage'
 import { NewIdeaPage } from './pages/NewIdeaPage'
+import { SignInPage } from './pages/SignInPage'
 import { SignUpPage } from './pages/SingUpPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
 import './styles/global.scss'
@@ -16,6 +17,7 @@ export const App = () => {
           <Route element={<Layout />}>
             <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
             <Route path={routes.getSignUpRoute()} element={<SignUpPage />} />
+            <Route path={routes.getSignInRoute()} element={<SignInPage />} />
             <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
             <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
           </Route>
