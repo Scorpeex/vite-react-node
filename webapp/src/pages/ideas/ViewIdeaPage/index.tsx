782a9edad8dc95e7706da82e8501724c86d74aa4
Scorpeex <argentum.nrj@gmail.com>
2025-05-29 19:00:39 +0700

    Add favicon and like icon

--- webapp/src/pages/ideas/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ideas/ViewIdeaPage/index.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom'
 import { Alert } from '../../../components/Alert'
 import { Button, LinkButton } from '../../../components/Button'
 import { FormItems } from '../../../components/FormItems'
+import { Icon } from '../../../components/Icon'
 import { Segment } from '../../../components/Segment'
 import { useForm } from '../../../lib/form'
 import { withPageWrapper } from '../../../lib/pageWrapper'
@@ -40,7 +41,7 @@ const LikeButton = ({ idea }: { idea: NonNullable<TrpcRouterOutput['getIdea']['i
         void setIdeaLike.mutateAsync({ ideaId: idea.id, isLikedByMe: !idea.isLikedByMe })
       }}
     >
-      {idea.isLikedByMe ? 'Unlike' : 'Like'}
+      <Icon size={32} className={css.likeIcon} name={idea.isLikedByMe ? 'likeFilled' : 'likeEmpty'} />
     </button>
   )
 }
@@ -78,6 +79,7 @@ export const ViewIdeaPage = withPageWrapper({
     me: ctx.me,
     showLoaderOnFetching: false,
   }),
+  title: ({ idea }) => idea.name,
 })(({ idea, me }) => (
   <Segment title={idea.name} description={idea.description}>
     <div className={css.createdAt}>Created At: {format(idea.createdAt, 'yyyy-MM-dd')}</div>
